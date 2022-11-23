3001a577597aac27b7f7c1bab80c28ede3e36799
JoshMorrison99 <josh.morrison1999@gmail.com>
2022-11-23 12:57:22 -0500

    added option to split url parameters

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-VERSION = '0.0.5'
+VERSION = '0.0.6'
 DESCRIPTION = 'Get URL Parameters'
 LONG_DESCRIPTION = 'A tools that takes URLs from stdin and outputs URLs with parameters.'
 
