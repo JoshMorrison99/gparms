919c1a915bfb37d29d94b9588141eb3496645ca2
JoshMorrison99 <josh.morrison1999@gmail.com>
2022-11-23 13:41:49 -0500

    scope option was too slow and not correct, it was removed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-VERSION = '0.0.7'
+VERSION = '0.0.8'
 DESCRIPTION = 'Get URL Parameters'
 LONG_DESCRIPTION = 'A tools that takes URLs from stdin and outputs URLs with parameters.'
 
